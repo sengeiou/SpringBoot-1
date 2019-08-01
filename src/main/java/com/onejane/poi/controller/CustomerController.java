3e8674a296edc2725fb244b442264b48df4b37da
OneJane <15806204096@163.com>
2019-08-01 17:17:25 +0800

    swagger

--- src/main/java/com/onejane/poi/controller/CustomerController.java
+++ src/main/java/com/onejane/poi/controller/CustomerController.java
@@ -1,6 +1,10 @@
 package com.onejane.poi.controller;
 
 import com.onejane.poi.service.CustomerService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+@Api(tags = "测试Excel")
 @RestController
 @RequestMapping("/test/")
 public class CustomerController {
@@ -15,9 +20,15 @@ public class CustomerController {
     @Autowired
     private CustomerService customerService;
 
+    @ApiOperation(value = "导入", notes = "导入")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "file", value = "文件", paramType = "query", dataType = "file"),
+            @ApiImplicitParam(name = "note", value = "备注", paramType = "query", dataType = "String")
+    })
     @PostMapping("/import")
-    public boolean addUser(@RequestParam("file") MultipartFile file) {
+    public boolean addUser(@RequestParam("file") MultipartFile file,String note) {
         boolean a = false;
+        System.out.println(note);
         String fileName = file.getOriginalFilename();
         try {
             a = customerService.batchImport(fileName, file);
